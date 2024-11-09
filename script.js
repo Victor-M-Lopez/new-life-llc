91bc5fbe11f7bdb5d5439085b9e3e27e3855fcdc
Victor Lopez <vic.m.lopez114@gmail.com>
2024-11-08 20:39:51 -0500

    added screen size responsiveness

---
+++ script.js
@@ -0,0 +1,9 @@
+navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+	if (window.scrollY > 100) {
+		navBar.classList.add('solid');
+	} else {
+		navBar.classList.remove('solid');
+	}
+});

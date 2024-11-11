cf1c5a462e782087dca85576c503549944e65611
Victor Lopez <vic.m.lopez114@gmail.com>
2024-11-11 00:09:05 -0500

    added new max width changes

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', function () {
 		navBar.classList.remove('solid');
 	}
 });
+
+const exitBtn = document.querySelector('#exit-btn');
+const navModal = document.querySelector('#nav_modal');
+let hamburgerBtn = document.querySelector('#hamburger_menu');
+let modalLinks = document.querySelectorAll('.nav_modal--list');
+
+for (let i = 0; i < modalLinks.length; i++) {
+	modalLinks[i].addEventListener('click', function () {
+		navModal.classList.remove('show_modal');
+	});
+}
+
+hamburgerBtn.addEventListener('click', function () {
+	navModal.classList.add('show_modal');
+});
+
+exitBtn.addEventListener('click', function () {
+	navModal.classList.remove('show_modal');
+});
